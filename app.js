e8b18c0f1c2e83e3c6a63501346b2b8d7dfc0269
Saptarshi Chatterjee <diesel@falcon.local>
2018-10-19 22:53:37 -0500

    works

--- app.js
+++ app.js
@@ -3,26 +3,40 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+const https = require('https');
+var kafka = require('kafka-node'),
+    Consumer = kafka.Consumer,
+    client = new kafka.Client(),
+    consumer = new Consumer(client,
+        [{ topic: 'test', offset: 0}],
+        {
+            autoCommit: false
+        }
+    );
 
 app.use(express.static(__dirname + '/node_modules'));
 app.get('/', function(req, res,next) {
     res.sendFile(__dirname + '/index.html');
 });
 
-
 io.on('connection', function(client) {
     console.log('Client connected...');
 
     client.on('join', function(data) {
 	        console.log(data);
-	    });
+	});
+
+	consumer.on('message', function (message) {
+		console.log('consumer message');
+    	console.log(message);
+	});
 
     client.on('messages', function(data) {
-		client.emit('broad', data);
-//		client.broadcast.emit('broad',data);
-           //client.broadcast.emit('broad',data);
+		consumer.on('message', function (message) {
+			client.broadcast.emit('broad',data);
+   			client.emit('broad', data);
+		});
     });
-
 })
 
 server.listen(4200);
